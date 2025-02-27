4edddd97edc41ee465c49446ec51407d94fe2067
Jeet <jeetpatil3333@gmail.com>
2025-02-27 20:37:35 +0530

    Meetings

--- src/components/Meetings.js
+++ src/components/Meetings.js
@@ -60,7 +60,7 @@ const Meetings = () => {
 
     const fetchUsers = async () => {
         try {
-            const response = await axios.get('http://localhost:5000/api/users', axiosConfig);
+            const response = await axios.get('http://localhost:5000/api/meetings/users', axiosConfig);
             setUsers(response.data);
         } catch (error) {
             console.error('Error fetching users:', error);
